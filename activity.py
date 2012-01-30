22060a3063b2d6fd38d6b1cd8d44946170255af3
Gonzalo Odiard <gonzalo@laptop.org>
2012-01-30 10:46:39 -0300

    Remove import of deprecated ActivityToolbox
    Signed-off-by: Gonzalo Odiard <gonzalo@laptop.org>

--- activity.py
+++ activity.py
@@ -24,7 +24,6 @@ from gettext import gettext as _
 from sugar3.activity import activity
 from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.activity.widgets import ActivityButton
-from sugar3.activity.widgets import ActivityToolbox
 from sugar3.activity.widgets import TitleEntry
 from sugar3.activity.widgets import StopButton
 from sugar3.activity.widgets import ShareButton
