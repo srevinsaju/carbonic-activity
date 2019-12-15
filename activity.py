cb13423f2b106385a6889514af01a8d8e165d0a7
srevinsaju <srevin03@gmail.com>
2019-12-15 23:54:06 +0300

    Initial launch

--- activity.py
+++ activity.py
@@ -16,6 +16,8 @@
 
 """HelloWorld Activity: A case study for developing an activity."""
 
+
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -30,6 +32,8 @@ from sugar3.activity.widgets import StopButton
 from sugar3.activity.widgets import ShareButton
 from sugar3.activity.widgets import DescriptionItem
 
+from graphics import Graphics, FONT_SIZES
+from ccore import *
 
 class HelloWorldActivity(activity.Activity):
     """HelloWorldActivity class as specified in activity.info"""
@@ -75,6 +79,71 @@ class HelloWorldActivity(activity.Activity):
         toolbar_box.show()
 
         # label with the text, make the string translatable
-        label = Gtk.Label(_("Hello World!"))
-        self.set_canvas(label)
-        label.show()
+        center_in_panel = Gtk.Alignment.new(0.5, 0, 0, 0)
+        b = convertor("hexane")
+        a = chkBond(b)
+        graphics = Graphics()
+        graphics.add_text(
+            _(a[0]))
+        c = convertor("heptane")
+        d = chkBond(c)
+        graphics = Graphics()
+        first = ''            
+        self._first_entry = graphics.add_entry(first)
+        print("LOG: #56, ", self._first_entry, self._first_entry )
+        button = graphics.add_button(_("COMPUTE"), self.something )
+        button.show()
+
+        self.hold_hi3 = graphics.add_text(
+            _(" "), size="xx-large", bold=True)
+            
+        self.hold_hi2 = graphics.add_text(
+            _(" "), size="xx-large", bold=True)
+
+        self.hold_hi1 = graphics.add_text(
+            _(" "), size="xx-large", bold=True)
+
+        self.hold0 = graphics.add_text(
+            _("CH4"), size="xx-large", bold=True)
+            
+        self.hold_lo1 = graphics.add_text(
+            _(" "), size="xx-large", bold=True)
+            
+        self.hold_lo2 = graphics.add_text(
+            _(" "), size="xx-large", bold=True)
+ 
+        self.hold_lo3 = graphics.add_text(
+            _(" "), size="xx-large", bold=True)
+            
+
+        center_in_panel.add(graphics)
+        graphics.show()
+        self.set_canvas(center_in_panel)
+        center_in_panel.show()
+        
+    def something(self, button_inst):
+    	#print("yo", text, t1,t2,t3)
+    	#print("ya", self._first_entry.get_text())
+    	val = self._first_entry.get_text()
+    	c = convertor(val)
+    	d = chkBond(c)
+    	print(d)
+    	if len(d) == 1:
+
+            self.hold_hi3.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')
+            self.hold_hi2.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')
+            self.hold_hi1.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')
+            self.hold0.set_markup('<span size="xx-large"><b>' + d[0] + '</b></span>')
+            self.hold_lo1.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')
+            self.hold_lo2.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')
+            self.hold_lo3.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')		    
+    		
+    	else:
+            self.hold_hi3.set_markup('<span size="xx-large"><b>' + d[6] + '</b></span>')
+            self.hold_hi2.set_markup('<span size="xx-large"><b>' + d[5] + '</b></span>')
+            self.hold_hi1.set_markup('<span size="xx-large"><b>' + d[4] + '</b></span>')
+            self.hold0.set_markup('<span size="xx-large"><b>' + d[0] + '</b></span>')
+            self.hold_lo1.set_markup('<span size="xx-large"><b>' + d[1] + '</b></span>')
+            self.hold_lo2.set_markup('<span size="xx-large"><b>' + d[2] + '</b></span>') 
+            self.hold_lo2.set_markup('<span size="xx-large"><b>' + d[3] + '</b></span>')		     		
+		
