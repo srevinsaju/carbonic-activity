a34e7aa3a42b259645235c8a1f1e82d0d7cfa986
srevinsaju <srevin03@gmail.com>
2019-12-17 09:55:04 +0300

    Fix appearance

--- activity.py
+++ activity.py
@@ -52,10 +52,10 @@ from graphics import Graphics, FONT_SIZES
 from ccore import *
 
 class CarbonicActivity(activity.Activity):
-    
+
 
     def __init__(self, handle):
-        
+
         activity.Activity.__init__(self, handle)
 
         # we do not have collaboration features
@@ -91,23 +91,31 @@ class CarbonicActivity(activity.Activity):
         toolbar_box.toolbar.insert(stop_button, -1)
         stop_button.show()
 
+        # FONT DEFINITIONS
+        FONT_FACE = 'Monospace'
+        input_font = Pango.FontDescription('%s %d' % (FONT_FACE, int(FONT_SIZE * 1.2)))
+        entry_font = Pango.FontDescription('%s %d' % (FONT_FACE, int(FONT_SIZE * 10.0 )))
+
         self.set_toolbar_box(toolbar_box)
         toolbar_box.show()
 
         # label with the text, make the string translatable
         center_in_panel = Gtk.Alignment.new(0.5, 0, 0, 0)
         graphics = Graphics()
-        first = 'methane'            
+        first = 'methane'
         self._first_entry = graphics.add_entry(first)
+        self._first_entry.override_font(input_font)
+        self._spac1 = graphics.add_text(' ')
+
         print("LOG: #56, ", self._first_entry, self._first_entry )
         button = graphics.add_button(_("COMPUTE"), self.something )
+        self._spac2 = graphics.add_text(' ')
         button.show()
-        FONT_FACE = 'Monospace'
-        input_font = Pango.FontDescription('%s %d' % (FONT_FACE, int(FONT_SIZE * 1.2)))
+
         self.hold_hi3 = graphics.add_text(
             _(" "), size="xx-large", bold=True, justify=Gtk.Justification.CENTER)
         self.hold_hi3.override_font(input_font)
-            
+
         self.hold_hi2 = graphics.add_text(
             _(" "), size="xx-large", bold=True, justify=Gtk.Justification.CENTER)
         self.hold_hi2.override_font(input_font)
@@ -119,27 +127,28 @@ class CarbonicActivity(activity.Activity):
         self.hold0.override_font(input_font)
         self.hold_lo1 = graphics.add_text(
             _(" "), size="xx-large", bold=True, justify=Gtk.Justification.CENTER)
-            
+        self.hold_lo1.override_font(input_font)
         self.hold_lo2 = graphics.add_text(
             _(" "), size="xx-large", bold=True, justify=Gtk.Justification.CENTER)
- 
+        self.hold_lo2.override_font(input_font)
         self.hold_lo3 = graphics.add_text(
             _(" "), size="xx-large", bold=True, justify=Gtk.Justification.CENTER)
-            
+        self.hold_lo3.override_font(input_font)
 
         center_in_panel.add(graphics)
         graphics.show()
         self.set_canvas(center_in_panel)
         center_in_panel.show()
-        
+
     def something(self, button_inst):
-    	#print("yo", text, t1,t2,t3)
-    	#print("ya", self._first_entry.get_text())
-    	val = self._first_entry.get_text()
-    	c = convertor(val)
-    	d = chkBond(c)
-    	print(d)
-    	if len(d) == 1:
+        #print("yo", text, t1,t2,t3)
+        #print("ya", self._first_entry.get_text())
+        val = self._first_entry.get_text()
+        c = convertor(val)
+        d = chkBond(c)
+        displax(d)
+
+        if len(d) == 1:
 
             self.hold_hi3.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')
             self.hold_hi2.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')
@@ -147,14 +156,13 @@ class CarbonicActivity(activity.Activity):
             self.hold0.set_markup('<span size="xx-large"><b>' + d[0] + '</b></span>')
             self.hold_lo1.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')
             self.hold_lo2.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')
-            self.hold_lo3.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')		    
-    		
-    	else:
+            self.hold_lo3.set_markup('<span size="xx-large"><b>' + "  " + '</b></span>')
+
+        else:
             self.hold_hi3.set_markup('<span size="xx-large"><b>' + d[6] + '</b></span>')
             self.hold_hi2.set_markup('<span size="xx-large"><b>' + d[5] + '</b></span>')
             self.hold_hi1.set_markup('<span size="xx-large"><b>' + d[4] + '</b></span>')
             self.hold0.set_markup('<span size="xx-large"><b>' + d[0] + '</b></span>')
             self.hold_lo1.set_markup('<span size="xx-large"><b>' + d[1] + '</b></span>')
-            self.hold_lo2.set_markup('<span size="xx-large"><b>' + d[2] + '</b></span>') 
-            self.hold_lo3.set_markup('<span size="xx-large"><b>' + d[3] + '</b></span>')		     		
-		
+            self.hold_lo2.set_markup('<span size="xx-large"><b>' + d[2] + '</b></span>')
+            self.hold_lo3.set_markup('<span size="xx-large"><b>' + d[3] + '</b></span>')
